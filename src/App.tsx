0740aa8267fb65ee418bdd60548aa7f0ab494c8f
mulukumoise-bit <mulukumoise@gmail.com>
2026-09-14 11:15:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div style={{ minHeight: '100vh', backgroundColor: '#ffffff', display: 'flex', justifyContent: 'center', alignItems: 'center', fontFamily: 'sans-serif', color: '#64748b' }}>
-      <p>Projet réinitialisé. Tout a été supprimé.</p>
-    </div>
-  );
-}
 
