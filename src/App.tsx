43ad70e139002166daf002b90827e548f261ad81
mulukumoise-bit <mulukumoise@gmail.com>
2026-09-14 19:02:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,1 @@
-import React from 'react';
 
-export default function App() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Application en cours de réinitialisation...</h1>
-      <p>Prépare-toi à coller la nouvelle version !</p>
-    </div>
-  );
-}
